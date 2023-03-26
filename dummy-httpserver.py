e18e46e87e65f7802e51311434c425b006591789
Patrick Husson <phusson12@gwu.edu>
2023-03-26 09:17:28 -0400

    Client and server handle game, myGame queries

--- dummy-httpserver.py
+++ dummy-httpserver.py
@@ -13,55 +13,47 @@
 
 from bottle import request,  route,  run
 import json
+from tictactoe import TicTacToe
 import urllib.parse
 
-mycount = 0
+query_counter = 0
+game_id_counter = 2000
+all_games = {}
 
+# Note: the dummy server will handle a mismatch between the method and the
+# command but the real server won't...
 @route('<mypath:path>', method=['GET', 'POST'])
 def query_handler(mypath):
-    global mycount
+    global query_counter
 
-    mycount = mycount + 1
-    print(f"count={mycount}")
+    query_counter = query_counter + 1
+    print(f"Handling query {query_counter}")
 
     query = urllib.parse.parse_qs(request.query_string)
-    resp = {}
-    resp["code"] = "OK"
-
     query_type = query['type'][0]
-    if query_type == 'game':
-        # Check mandatory parameters
-        if 'teamId1' in query and \
-            'teamId2' in query and \
-            query['gameType'][0] == 'TTT':
-                # Check for optional parameters
-                board_size = 20 # Default
-                if 'boardSize' in query:
-                    board_size = query['boardSize']
-                target = 6 # Default
-                if 'target' in query:
-                    target = query['target']
-                print(f"Valid {query_type} command: boardSize={board_size}, target={target}")
 
+    if query_type == 'game':
+        resp = handle_create_game(query)
     elif query_type == 'myGames' or query_type == 'myOpenGames':
-        # No other parameters...
-        print(f"Valid {query_type} command")
-    elif query_type == 'move':
-        if 'gameId' in query and \
-            'teamId' in query and \
-            'move' in query:
-                # TODO - Make the checks for valid items
-                print(f"Valid {query_type} command")
-    elif query_type == 'moves':
-        if 'gameId' in query and \
-            'count' in query:
-            print(f"Valid {query_type} command")
-    elif query_type == 'boardString':
-        if 'gameId' in query:
-            print(f"Valid {query_type} command")
-    elif query_type == 'boardMap':
-        if 'gameId' in query:
-            print(f"Valid {query_type} command")
+        resp = handle_get_my_games(query)
+#        # No other parameters...
+#        print(f"Valid {query_type} command")
+#    elif query_type == 'move':
+#        if 'gameId' in query and \
+#            'teamId' in query and \
+#            'move' in query:
+#                # TODO - Make the checks for valid items
+#                print(f"Valid {query_type} command")
+#    elif query_type == 'moves':
+#        if 'gameId' in query and \
+#            'count' in query:
+#            print(f"Valid {query_type} command")
+#    elif query_type == 'boardString':
+#        if 'gameId' in query:
+#            print(f"Valid {query_type} command")
+#    elif query_type == 'boardMap':
+#        if 'gameId' in query:
+#            print(f"Valid {query_type} command")
     else:
         err_msg = f"This server does not handle the '{query_type}' command"
         resp["code"] = "FAIL"
@@ -69,6 +61,44 @@ def query_handler(mypath):
 
     return json.dumps(resp)
 
+
+def handle_create_game(query):
+    global game_id_counter
+    query_type = query['type'][0]
+
+    # Check mandatory parameters
+    if 'teamId1' in query and \
+        'teamId2' in query and \
+        query['gameType'][0] == 'TTT':
+            # Check for optional parameters
+            board_size = 20 # Default
+            if 'boardSize' in query:
+                board_size = int(query['boardSize'][0])
+            target = 6 # Default
+            if 'target' in query:
+                target = int(query['target'][0])
+
+            resp = {}
+            resp['code'] = "OK"
+            resp['gameid'] = game_id_counter
+            print(f"Valid {query_type} command: boardSize={board_size}, target={target}")
+            new_ttt = TicTacToe(board_size, target)
+            all_games[game_id_counter] = new_ttt
+            game_id_counter += 1
+    else:
+        resp = {}
+        resp["code"] = "FAIL"
+        resp["message"] = f"This server does not handle the '{query_type}' command"
+    return resp
+
+
+def handle_get_my_games(query):
+    # Much simpler than the real server's implementation - all games include the client
+    resp = {}
+    resp["code"] = "OK"
+    resp["games"] = list(all_games.keys())
+
+    return resp
 
 if __name__ == "__main__":
     run(host='localhost', port=8080, debug=True)
--- project-httpclient.py
+++ project-httpclient.py
@@ -11,40 +11,45 @@
 #
 # project_httpclient.py - HTTP client side of the game (our side)
 
+
 import ast
 import requests
 import urllib.parse
 
+
 # Using the default requests user agent caused errors with the class server:
 # 'Not Acceptable! An appropriate representation of the requested resource could
 # not be found on this server.'
+my_key = 0
+my_id = 0
+
+use_dummy = True
+dummy_id = '9999'
 dummy_ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'
+dummy_http_server = 'http://127.0.0.1:8080'
 
-if __name__ == '__main__':
+real_http_server = 'https://www.notexponential.com/aip2pgaming/api/index.php'
 
-    # Make sure API key and user ID are in memory
-    my_id = 0
-    my_key = 0
-    with open('token.txt') as f:
-        for line in f:
-            if not line.startswith("#"):
-                values = line.split(',')
-                my_id = values[0]
-                my_key = values[1].strip()
-                break
 
-    #http_server = "https://www.notexponential.com/aip2pgaming/api/index.php
-    http_server = 'http://127.0.0.1:8080'
+my_games = {}
+
+def create_new_game():
+    # TODO - Temporary - just going to hardcode some things for now
+    # - Client will be Player 1, Server will be Player 2
+    # - Board size and target will be for a standard game
+    board_size = 3
+    target_size = 3
 
     params = {}
     params['type'] = 'game'
-    params['teamId1'] = '1000'
-    params['teamId2'] = '1001'
+    params['teamId1'] = my_id
+    params['teamId2'] = dummy_id # TODO - Check if using dummy eventually...
     params['gameType'] = 'TTT'
-    # ...
+    params['boardSize'] = board_size
+    params['target'] = target_size
 
     query = urllib.parse.urlencode(params)
-    url = f"{http_server}?{query}"
+    url = f"{dummy_http_server}?{query}"
 
     payload={}
     headers = {
@@ -53,9 +58,55 @@ if __name__ == '__main__':
         'User-Agent': dummy_ua
     }
 
-    # Just replace "GET" with "POST" as necessary
-    response = requests.request("GET", url, headers=headers, data=payload)
+    raw_response = requests.request("POST", url, headers=headers, data=payload)
+    response = ast.literal_eval(raw_response.text)
 
-    print(response.text)
-    mydict = ast.literal_eval(response.text)
-    print(f"Response as JSON:{mydict}")
+    if ( response['code'] == 'OK'):
+        print(f"Created game {response['gameid']} successfully")
+    else:
+        print(f"Failure in creating game,  message={response['message']}")
+
+
+def get_my_games():
+    params = {}
+    params['type'] = 'myGames'
+
+    query = urllib.parse.urlencode(params)
+    url = f"{dummy_http_server}?{query}"
+
+    payload={}
+    headers = {
+        'x-api-key': my_key,
+        'userid': my_id,
+        'User-Agent': dummy_ua
+    }
+
+    raw_response = requests.request("GET", url, headers=headers, data=payload)
+    response = ast.literal_eval(raw_response.text)
+    if ( response['code'] == 'OK'):
+        mygames = response['games']
+        print(f"mygames={mygames}")
+    else:
+        print(f"Failure in creating game,  message={response['message']}")
+
+def play():
+    # TODO - Temporary - just create a new game by default
+    create_new_game()
+
+    # TODO - Temporary - want to see this grow over time
+    get_my_games()
+
+if __name__ == '__main__':
+
+    # Make sure API key and user ID are in memory
+    with open('token.txt') as f:
+        for line in f:
+            if not line.startswith("#"):
+                values = line.split(',')
+                my_id = values[0]
+                my_key = values[1].strip()
+                break
+
+    play()
+
+
